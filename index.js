ff31e5c7bd1447bfb51e16ff63f08d99fa4620e3
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-07 00:33:30 +0300

    attempted context lab

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
 /* Your Code Here */
+const createEmployeeRecord=(array)=> {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = (array) => {
+    return array.map(createEmployeeRecord)
+}
+
+const createTimeInEvent = (employeeRecord, dateStamp)=> {
+    const [date, time] = dateStamp.split(" ")
+    const [year, month, day] = date.split('-')
+    const [hour, minute] = time.split(':') 
+    
+    let record = {
+        type: "TimeIn",
+        hour: parseInt(time),
+        date: {
+            year: parseInt(year),
+            month: parseInt(month),
+            day: parseInt(day),
+        },
+    }
+    return record
+}
+
+const createTimeOutEvent = (dateStamp)=> {
+    const [date, time] = dateStamp.split(" ")
+    const [year, month, day] = date.split('-')
+    const [hour, minute] = time.split(':') 
+    let record = {}
+     record = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: {
+            year: parseInt(year),
+            month: parseInt(month),
+            day: parseInt(day),
+        },
+    }
+    return record
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
